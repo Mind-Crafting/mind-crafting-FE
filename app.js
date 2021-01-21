221cdc80231a241231dbbb543341cc6a654d816f
Ryan Carreras <ryan.m.carreras@gmail.com>
2021-01-21 15:23:49 -0800

    added player selection. 2 player limit (#10)

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-// import functions and grab DOM elements
+const players = document.getElementById("players");
 
-// initialize state
+players.addEventListener("click", () => {
 
-// set event listeners to update state and DOM
+  const selectedRadioButton = document.querySelector("input:checked");
+  console.log(selectedRadioButton.value);
+  const iframeSRC = selectedRadioButton.value;
+  const player1 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3000";
+  const player2 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3001";
+ 
+  if(iframeSRC === player1) document.getElementById("viewer").src = player1;
+  if(iframeSRC === player2) document.getElementById("viewer").src = player2;
+});
