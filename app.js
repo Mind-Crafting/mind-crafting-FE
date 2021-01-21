4cb11a747a7254953a93d06a845602c66e608d28
Ryan Carreras <ryan.m.carreras@gmail.com>
2021-01-21 15:33:15 -0800

    Dev branch ready to implement (#11)
    * added simple bootstrap (#1)
    
    * Feature/layout (#3)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * Fix/http to https (#4)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * bug fix for http to https
    
    * Update README.md
    
    * Update README.md
    
    * Fix/http to https (#5)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * bug fix for http to https
    
    * bug fix #2 for http to https
    
    * bug fix (#6)
    
    * Fix/iframe fix (#7)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * Fix/iframe fix (#8)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * Fix/iframe fix (#9)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * added player selection. 2 player limit (#10)

---
+++ app.js
@@ -0,0 +1,13 @@
+const players = document.getElementById("players");
+
+players.addEventListener("click", () => {
+
+  const selectedRadioButton = document.querySelector("input:checked");
+  console.log(selectedRadioButton.value);
+  const iframeSRC = selectedRadioButton.value;
+  const player1 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3000";
+  const player2 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3001";
+ 
+  if(iframeSRC === player1) document.getElementById("viewer").src = player1;
+  if(iframeSRC === player2) document.getElementById("viewer").src = player2;
+});
