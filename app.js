fd0f5b562a90795d5353216b20dd888e986a8606
Ryan Carreras <ryan.m.carreras@gmail.com>
2021-01-22 12:17:28 -0800

    Fix/server change update (#14)
    * Dev branch ready to implement (#11)
    
    * added simple bootstrap (#1)
    
    * Feature/layout (#3)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * Fix/http to https (#4)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * bug fix for http to https
    
    * Update README.md
    
    * Update README.md
    
    * Fix/http to https (#5)
    
    * added simple bootstrap
    
    * layout finished
    
    * tweaked header
    
    * file cleanup
    
    * bug fix for http to https
    
    * bug fix #2 for http to https
    
    * bug fix (#6)
    
    * Fix/iframe fix (#7)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * Fix/iframe fix (#8)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * Fix/iframe fix (#9)
    
    * bug fix
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * smol tweak for testing http to https redirect
    
    * added player selection. 2 player limit (#10)
    
    * updated links due to server change
    
    * updated links due to server change
    
    * updated links due to server change

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ players.addEventListener("click", () => {
   const selectedRadioButton = document.querySelector("input:checked");
   console.log(selectedRadioButton.value);
   const iframeSRC = selectedRadioButton.value;
-  const player1 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3000";
-  const player2 = "http://ec2-3-129-43-241.us-east-2.compute.amazonaws.com:3001";
+  const player1 = "http://ec2-3-138-151-182.us-east-2.compute.amazonaws.com:3000";
+  const player2 = "http://ec2-3-138-151-182.us-east-2.compute.amazonaws.com:3001";
  
   if(iframeSRC === player1) document.getElementById("viewer").src = player1;
   if(iframeSRC === player2) document.getElementById("viewer").src = player2;
